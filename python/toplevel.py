1c9dbc53f6028bf04c095db57ea11e4d75f8e771
danko1981 <145504669+danko1981@users.noreply.github.com>
2026-04-05 21:51:46 +0200

    Implement change_tool function in toplevel.py
    Add change_tool function to interface with remap.py

---
+++ python/toplevel.py
@@ -0,0 +1,5 @@
+import remap
+
+def change_tool(self, **words):
+    # Questa funzione fa da ponte tra LinuxCNC e la logica nel file remap.py
+    return remap.manual_change_with_probe(self, **words)
